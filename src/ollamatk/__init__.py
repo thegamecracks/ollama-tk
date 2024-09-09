36052aa3651674c1e25ddff2c076e925d42cb113
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-09-09 10:19:20 -0400

    feat: export all our classes and functions from ollamatk

--- src/ollamatk/__init__.py
+++ src/ollamatk/__init__.py
@@ -1,2 +1,10 @@
+from .about import TkAboutWindow
 from .app import TkApp
+from .chat import TkChat, TkChatButtons, TkChatControls, TkLiveControls, TkChatMenu
 from .event_thread import EventThread
+from .http import HTTPClient
+from .logging import LogStore, TkAppLogHandler, TkLogWindow, configure_logging
+from .messages import Message, TkMessageFrame, TkMessageList, load_message_icons
+from .scrollable_frame import ScrollableFrame
+from .settings import Settings, TkSettingsControls
+from .wrap_label import WrapLabel
