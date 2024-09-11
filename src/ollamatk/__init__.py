c143cc83a08a212d21a464348ede7bc2ee24ca44
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-09-11 00:09:26 -0400

    feat: export StreamingChatHandler and HTTP typehints

--- src/ollamatk/__init__.py
+++ src/ollamatk/__init__.py
@@ -1,8 +1,15 @@
 from .about import TkAboutWindow
 from .app import TkApp
-from .chat import TkChat, TkChatButtons, TkChatControls, TkLiveControls, TkChatMenu
+from .chat import (
+    StreamingChatHandler,
+    TkChat,
+    TkChatButtons,
+    TkChatControls,
+    TkLiveControls,
+    TkChatMenu,
+)
 from .event_thread import EventThread
-from .http import HTTPClient
+from .http import DoneStreamingChat, HTTPClient, Message, StreamingChat
 from .logging import LogStore, TkAppLogHandler, TkLogWindow, configure_logging
 from .messages import Message, TkMessageFrame, TkMessageList, load_message_icons
 from .scrollable_frame import ScrollableFrame
